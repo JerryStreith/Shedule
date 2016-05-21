8f4e3e67d2df885026d1e6d662900d4531764448
Anastasia Sharnenkova <nastia_lion24@ukr.net>
2016-05-21 22:37:17 +0300

    Delete prev functionality

--- Shedule/MainWindow.xaml.cs
+++ Shedule/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace Shedule
 {
@@ -36,29 +41,25 @@ namespace Shedule
         {
             InitializeComponent();
 
-            schedule.Add(new List<ScheduleItem>());
-            schedule[0].Add(new ScheduleItem("1", "Безпека програм та даних", "лекція", "ауд.40"));
-            schedule[0].Add(new ScheduleItem("2", "Безпека програм та даних", "лаба", "оц-3"));
+            ServicePointManager.Expect100Continue = false;
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var values = new NameValueCollection();
+                    values["group"] = "СП-12-1д";
+                    var response = client.UploadValues("http://zschedule.gorlachov.com/libs/get_rings.php", values);
+                    var responseString = Encoding.UTF8.GetString(response);
 
-            schedule.Add(new List<ScheduleItem>());
-            schedule[1].Add(new ScheduleItem("1", "Програмування інтернет", "лекція", "ауд.40"));
-            schedule[1].Add(new ScheduleItem("2", "Програмування інтернет", "лаба", "оц-3"));
-            schedule[1].Add(new ScheduleItem("3", "Аналіз вимог до ПЗ", "лекція", "ауд.40"));
+                    List<Ring> myDeserializedObjList = (List<Ring>)Newtonsoft.Json.JsonConvert.DeserializeObject(responseString, typeof(List<Ring>));
 
-            schedule.Add(new List<ScheduleItem>());
-            schedule[2].Add(new ScheduleItem("1", "Архітектура та проектування ПЗ", "лекція", "ауд.40"));
-            schedule[2].Add(new ScheduleItem("2", "Архітектура та проектування ПЗ", "лаба", "оц-3"));
-            schedule[2].Add(new ScheduleItem("3", "Аналіз вимог до ПЗ", "лаба", "ауд.40"));
-
-            schedule.Add(new List<ScheduleItem>());
-            schedule[3].Add(new ScheduleItem("1", "Безпека програм та даних", "лекція", "ауд.40"));
-            schedule[3].Add(new ScheduleItem("2", "Безпека програм та даних", "лаба", "оц-3"));
-            schedule[3].Add(new ScheduleItem("3", "Програмування інтернет", "лекція", "ауд.40"));
-
-            schedule.Add(new List<ScheduleItem>());
-            schedule[4].Add(new ScheduleItem("1", "Аналіз вимог до ПЗ", "лекція", "ауд.40"));
-            schedule[4].Add(new ScheduleItem("2", "Архітектура та проектування ПЗ", "лекція", "оц-3"));
-            schedule[4].Add(new ScheduleItem("3", "Аналіз вимог до ПЗ", "лаба", "оц-3"));
+                    MessageBox.Show(responseString);
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
 
             currentDate = DateTime.Now;
 
@@ -92,20 +93,12 @@ namespace Shedule
             fridayTxtBox.Add(frsh4);
             fridayTxtBox.Add(frsh5);
 
-            faculties.Add("MФ");
-            faculties.Add("ФАСУВ");
-            faculties.Add("ФБВР");
-            faculties.Add("ФЕЕ");
-            faculties.Add("ФЕУ");
-
             days.Add(mondayCanvas);
             days.Add(tuesdayCanvas);
             days.Add(wednesdayCanvas);
             days.Add(thursdayCanvas);
             days.Add(fridayCanvas);
             days.Add(sundayCanvas);
-
-            facultyComboBox.ItemsSource = faculties;
 
             DislayCurrentDay();
         }
@@ -154,146 +147,25 @@ namespace Shedule
 
         private void facultyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string faculty = "";
             if (facultyComboBox.SelectedIndex != -1)
             {
-                faculty = facultyComboBox.SelectedValue.ToString();
-
-                switch (faculty)
-                {
-                    case "МФ":
-                        departments.Clear();
-
-                        break;
-                    case "ФАСУВ":
-                        departments.Clear();
-                        departments.Add("ПЗАС");
-                        departments.Add("АУТП");
-                        departments.Add("МН");
-                        departments.Add("ЕС");
-                        break;
-                    case "ФБВР":
-                        departments.Clear();
-
-                        break;
-                    case "ФЕЕ":
-                        departments.Clear();
-
-                        break;
-                    case "ФЕУ":
-                        departments.Clear();
-
-                        break;
-                }
+                //выбираем факультет
             }
-            DepartmentComboBox.ItemsSource = departments;
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void DepartmentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string department = "";
             if (DepartmentComboBox.SelectedIndex != -1)
             {
-                department = DepartmentComboBox.SelectedValue.ToString();
-                switch (department)
-                {
-                    case "ПЗАС":
-                        groups = new List<string>();
-                        groups.Add("СП-12-1с");
-                        groups.Add("СП-12-1м");
-                        groups.Add("СП-12-1");
-                        groups.Add("СП-13-1");
-                        groups.Add("СП-13-2");
-                        groups.Add("СП-14-1");
-                        groups.Add("СП-14-2");
-                        groups.Add("СП-15-1");
-                        groups.Add("СП-15-2");
-
-                        break;
-                    case "АУТП":
-                        groups = new List<string>();
-                        groups.Add("АУТП-12-1с");
-                        groups.Add("АУТП-12-1м");
-                        groups.Add("АУТП-12-1");
-                        groups.Add("АУТП-13-1");
-                        groups.Add("АУТП-13-2");
-                        groups.Add("АУТП-14-1");
-                        groups.Add("АУТП-14-2");
-                        groups.Add("АУТП-15-1");
-                        groups.Add("АУТП-15-2");
-
-                        break;
-                    case "МН":
-                        groups = new List<string>();
-                        groups.Add("МН-12-1с");
-                        groups.Add("МН-12-1м");
-                        groups.Add("МН-12-1");
-                        groups.Add("МН-13-1");
-                        groups.Add("МН-13-2");
-                        groups.Add("МН-14-1");
-                        groups.Add("МН-14-2");
-                        groups.Add("МН-15-1");
-                        groups.Add("МН-15-2");
-
-                        break;
-                    case "ЭС":
-                        groups = new List<string>();
-                        groups.Add("ЭС-12-1с");
-                        groups.Add("ЭС-12-1м");
-                        groups.Add("ЭС-12-1");
-                        groups.Add("ЭС-13-1");
-                        groups.Add("ЭС-13-2");
-                        groups.Add("ЭС-14-1");
-                        groups.Add("ЭС-14-2");
-                        groups.Add("ЭС-15-1");
-                        groups.Add("ЭС-15-2");
-
-                        break;
-                }
-                groupComboBox.ItemsSource = groups;
-
+                //по выбраному факультеты выбираем группу
             }
         }
 
         private void groupComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string group = "";
             if (groupComboBox.SelectedIndex != -1)
             {
-                group = groupComboBox.SelectedValue.ToString();
-                if (group == "СП-12-1")
-                {
-                    for (int i = 0; i < schedule[0].Count; i++)
-                    {
-                        modayTxtBox[i].Visibility = Visibility.Visible;
-                        modayTxtBox[i].Text = schedule[0][i].ScheduleItemToString();
-                    }
-                    for (int i = 0; i < schedule[1].Count; i++)
-                    {
-                        tuesdayTxtBox[i].Visibility = Visibility.Visible;
-                        tuesdayTxtBox[i].Text = schedule[1][i].ScheduleItemToString();
-                    }
-                    for (int i = 0; i < schedule[2].Count; i++)
-                    {
-                        wednesdayTxtBox[i].Visibility = Visibility.Visible;
-                        wednesdayTxtBox[i].Text = schedule[2][i].ScheduleItemToString();
-                    }
-                    for (int i = 0; i < schedule[3].Count; i++)
-                    {
-                        thursdayTxtBox[i].Visibility = Visibility.Visible;
-                        thursdayTxtBox[i].Text = schedule[3][i].ScheduleItemToString();
-                    }
-                    for (int i = 0; i < schedule[4].Count; i++)
-                    {
-                        fridayTxtBox[i].Visibility = Visibility.Visible;
-                        fridayTxtBox[i].Text = schedule[4][i].ScheduleItemToString();
-                    }
-                }
+                //по выбранной группе получаем расписание на всю неделю
             }
         }
 
